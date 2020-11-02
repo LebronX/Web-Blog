9ee41de74cf36a40194c2cc2dc36115ab5187913
Xuan Xie <lebron716@outlook.com>
2020-11-02 14:07:52 +0100

    blog list

---
+++ blog/components/Author.js
@@ -0,0 +1,11 @@
+import {Avatar,Divider} from 'antd'
+
+const Author = ()=>{
+    return(
+        <div className="author-div comm-box">
+            <div>
+                <Avatar size="100" src="../public/XuanXie_Pic.jpeg" />
+            </div>
+        </div>
+    )
+}
